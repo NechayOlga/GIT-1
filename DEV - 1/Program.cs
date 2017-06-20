cda225a0268578216938770d8368946afbb57404
Nechay Olga <olganechay.bsu@gmail.com>
2017-06-20 15:56:24 +0300

    change tabs2

--- DEV - 1/Program.cs
+++ DEV - 1/Program.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace SwapNumbers
 {
-		class Program
-		{
-				static void Main(string[] args)
-				{
-						for (int n = 0; n <= 100; n++)
-						{
-								String numbers = n % 3 == 0 ? "3*" + n/3 : n.ToString();
-								Console.Write(n + " ");
-						}
-						Console.ReadKey();
-				}
-		}
+    class Program
+{
+    static void Main(string[] args)
+    {
+       for (int n = 0; n <= 100; n++)
+         {
+            String numbers = n % 3 == 0 ? "3*" + n/3 : n.ToString();
+            Console.Write(n + " ");
+         }
+        Console.ReadKey();
+      }
+    }
 }
