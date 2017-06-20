86cb3fabe3f2e9cf284e1aa61018129db2b6883e
Nechay Olga <olganechay.bsu@gmail.com>
2017-06-20 15:16:31 +0300

    Change numbers

---
+++ DEV - 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			for (int n = 0; n <= 100; n++)
+
+			{
+				String numbers = n % 3 == 0 ? "3*" + n / 3 : n.ToString();
+				Console.Write(numbers + " ");
+			}
+		
+			Console.ReadKey();
+		}
+	}
+}
