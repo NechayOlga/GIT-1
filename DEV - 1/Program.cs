5008df740c18988ac6c7d53d29cbebfab4a49ecd
Nechay Olga <olganechay.bsu@gmail.com>
2017-06-20 17:25:21 +0300

    fix code style

--- DEV - 1/Program.cs
+++ DEV - 1/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 
-
 namespace SwapNumbers
 {
     class Program
     {
-      static void Main(string[] args)
-      {
-        String numbers;
-       for ( int n = 0; n <= 100; n++)
-       {
-         numbers = n % 3 == 0 ? "3*" + n / 3 : n.ToString();
-         Console.Write(numbers + " ");
-       }
-        Console.ReadKey();
-      }
+        // Entrypoint programm
+        static void Main(string[] args)
+        {
+            String numbers;
+            for (int n = 0; n <= 100; n++)
+            {
+                numbers = n % 3 == 0 ? "3*" + n / 3 : n.ToString();
+                Console.Write(numbers + " ");
+            }
+            Console.ReadKey();
+        }
     }
 }
