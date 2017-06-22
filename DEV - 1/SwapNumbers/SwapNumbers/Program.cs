205f77f9d40387fd252f47fc28e92242a99de45d
Nechay Olga <olganechay.bsu@gmail.com>
2017-06-22 17:05:42 +0300

    Change numbers DEV-2 Tutti - Frutti

--- DEV - 1/SwapNumbers/SwapNumbers/Program.cs
+++ DEV - 1/SwapNumbers/SwapNumbers/Program.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 
-namespace SwapNumbers
+namespace SwapNumbersToTuttiFrutti
 {
     class Program
     {
         // Entrypoint programm
         static void Main(string[] args)
         {
-            String numbers;
             for (int n = 0; n <= 100; n++)
             {
-                numbers = n % 3 == 0 ? "3*" + n / 3 : n.ToString();
+                String numbers = n.ToString();
+                if (n % 3 == 0)
+                {
+                    numbers = "Tutti";
+                }
+                if (n % 5 == 0)
+                {
+                    numbers = "Frutti";
+                }
+                if ((n % 3 == 0) && (n % 5 == 0))
+                {
+                    numbers = "Tutti-Frutti";
+                }
+
                 Console.Write(numbers + " ");
             }
             Console.ReadKey();
         }
     }
 }
+
