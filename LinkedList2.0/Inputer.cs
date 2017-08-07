e86ec330ed4396a256d37e1304c787f21fab614a
Nechay Olga <olganechay.bsu@gmail.com>
2017-08-07 17:25:24 +0300

    Create two methods for linkedList.

---
+++ LinkedList2.0/Inputer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList2
+{
+    class Inputer
+    {
+        public int Input()
+        {
+            int n = int.Parse(Console.ReadLine());
+            return n;
+        }
+    }
+}
