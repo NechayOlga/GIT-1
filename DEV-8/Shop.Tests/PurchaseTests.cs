8ecf3a8c4bbc68895b63b3c967253cb53025da56
Nechay Olga <olganechay.bsu@gmail.com>
2017-08-27 19:05:49 +0300

    Unit-test for Purchase class was added

---
+++ DEV-8/Shop.Tests/PurchaseTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shop.Tests
+{
+    [TestClass]
+    public class PurchaseTests
+    {
+        [TestMethod]
+        public void OutputPurchase_type_name_1_1_1returned()
+        {
+            //arrange
+            const string type = "type";
+            const string name = "name";
+            const int quantity = 1;
+            const decimal price = 1.1M;
+
+            //act
+            Purchase purchase = new Purchase(type, name, quantity, price);
+            string actual = Convert.ToString("\nTYPE: " + " " + purchase.Type + "\nNAME : " + " " + purchase.Name + "\nQUANTITY: " + " " + purchase.Quantity + "\nPRICE: " + " " + purchase.Price + "\n");
+            string expected = String.Empty;
+            expected = purchase.OutputPurchase();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
