82c280968bf16af82aa22d1b438d7ea5db24c5b1
Nechay Olga <olganechay.bsu@gmail.com>
2017-09-28 11:54:05 +0300

    Comments for class Entrypoint were added

--- DEV-9/RandomReplacementPartsOfLines/Entrypoint.cs
+++ DEV-9/RandomReplacementPartsOfLines/Entrypoint.cs
@@ -12,12 +12,15 @@ namespace RandomReplacementPartsOfLines
             string initialLine = INITIAL_LINE;
             string replacingLine = REPLACING_LINE;
 
+            //Creating of replaced and replacing sublines
             LinesBuilder sublinesBuilder = new LinesBuilder(initialLine, replacingLine);
             string replacedSubline = sublinesBuilder.BuildReplacedSubline();
             string replacingSubline = sublinesBuilder.BuildReplacingSubline();
 
+            //Creating of result line with replacing parts
             string resultLine = sublinesBuilder.SublinesReplacing(initialLine, replacedSubline, replacingSubline);
 
+            //Outputing of initial, replacing and result lines
             Console.WriteLine(initialLine);
             Console.WriteLine(replacingLine);
             Console.WriteLine(resultLine);
