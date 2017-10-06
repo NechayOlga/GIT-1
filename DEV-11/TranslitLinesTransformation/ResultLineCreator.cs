f77903d195780b057b065c5ffa3adc2e8027f6bd
Nechay Olga <olganechay.bsu@gmail.com>
2017-10-06 17:47:42 +0300

    Project to transform symbols to traslit.

---
+++ DEV-11/TranslitLinesTransformation/ResultLineCreator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace TranslitLinesTransformation
+{
+    /// <summary>
+    /// enum LanguageChoice contains
+    /// markers of language
+    /// </summary>
+    public enum LanguageChoice
+    {
+        Latin,
+        Cyrillics,
+        NonExactlyLanguage,
+        EmptyLine
+    }
+    /// <summary>
+    /// Class is for creating the result line
+    /// with relaced symbols.
+    /// </summary>
+    class ResultLineCreator
+    {
+        public string InputedLine
+        { get; private set; }
+        public ResultLineCreator(string inputedLine)
+        {
+            InputedLine = inputedLine;
+        }
+        /// <summary>
+        /// Method is for choose the rigth language.
+        /// </summary>
+        /// <param name="CyrillicsToLatin">is dictionary contains cyrrillics symbols as keys and latin as values</param>
+        /// <param name="LatinToCyrillics">is dictionary contains latin symbols as keys and cyrillics as values</param>
+        /// <returns>marker of relevant language</returns>
+        public LanguageChoice Language(Dictionary<string , string> CyrillicsToLatin, Dictionary<string, string> LatinToCyrillics)
+        {
+            LanguageChoice language = LanguageChoice.Latin;
+            foreach (KeyValuePair<string, string> cyrillicsToLatin in CyrillicsToLatin)
+            {
+                if (InputedLine.Contains(cyrillicsToLatin.Key))
+                {
+                    language = LanguageChoice.Cyrillics;
+                }
+                foreach (KeyValuePair<string, string> latinToCyrillics in LatinToCyrillics)
+                {
+                    if (InputedLine.Contains(latinToCyrillics.Key)&&InputedLine.Contains(cyrillicsToLatin.Key))
+                    {
+                        language = LanguageChoice.NonExactlyLanguage;
+                    }
+                }
+            }
+             return language;
+        }
+
+        /// <summary>
+        /// Method replaces symbols in inputed line.
+        /// </summary>
+        /// <param name="transformatedSymbols">dictionary of transformated symbols</param>
+        /// <returns>line with replaced simbols</returns>
+        public string ReplacedLine(Dictionary<string, string> transformatedSymbols)
+        {
+            foreach(KeyValuePair<string, string> kvp in transformatedSymbols)
+            {
+                InputedLine = InputedLine.Replace(kvp.Key, kvp.Value);
+            }
+            return InputedLine;
+        }
+    }
+}
---
+++ DEV-11/TranslitLinesTransformation/SymbolsStorageCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TranslitLinesTransformation
+{
+    /// <summary>
+    /// Class is for filling dictionary.
+    /// </summary>
+    class SymbolsStorageCreator
+    {
+        /// <summary>
+        /// Method fills dictionatry with relevant symbols.
+        /// </summary>
+        /// <param name="transformatedSymbols">line of symbols that should be transformated</param>
+        /// <param name="transformatingSymbols">line of symbols that should be transformating</param>
+        /// <returns></returns>
+        public Dictionary<string, string> CreateSymbolsStorage(string transformatedSymbols, string transformatingSymbols)
+        {
+            Dictionary<string,string> SymbolsStorage = new Dictionary<string, string>();
+            Encoding enc = Encoding.GetEncoding(1251);
+            using (StreamReader transformatedSymbol = new StreamReader(transformatedSymbols, enc),
+                                transformatingSymbol = new StreamReader(transformatingSymbols, enc))
+            {
+                while ((!transformatedSymbol.EndOfStream) || (!transformatingSymbol.EndOfStream))
+                {
+                    SymbolsStorage.Add(transformatedSymbol.ReadLine(), transformatingSymbol.ReadLine());
+                }
+            }
+            return SymbolsStorage;
+        }
+    }
+}
