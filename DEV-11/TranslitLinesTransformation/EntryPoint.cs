2a86375bd045845e0f30d72bbda1e06012d2bb05
Nechay Olga <olganechay.bsu@gmail.com>
2017-10-15 14:14:39 +0300

    Task DEV-11 was added

---
+++ DEV-11/TranslitLinesTransformation/EntryPoint.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TranslitLinesTransformation
+{
+  /// <summary>
+  /// The entry point of programm.
+  /// </summary>
+  class EntryPoint
+  {
+    private const string cyrillicsToLatin = @"e:\TAT\GIT-1\DEV-11\TranslitLinesTransformation\CyrillicsToLatin.txt";
+    private const string latinFromCyrillic = @"e:\TAT\GIT-1\DEV-11\TranslitLinesTransformation\LatinFromCyrillics.txt";
+    private const string latinToCyrillics = @"e:\TAT\GIT-1\DEV-11\TranslitLinesTransformation\LatinToCyrillics.txt";
+    private const string cyrillicsFromLatin = @"e:\TAT\GIT-1\DEV-11\TranslitLinesTransformation\CyrillicsFromLatin.txt";
+
+    const string INVALIDLINE = "Line is invalid. Line must be olny cyrillics or only latin";
+    const string FILENOTFOUND = "The text file isn't found.";
+    const string NONTRASLITLINE = "This line can't be transformed to translit";
+    
+    /// <summary>
+    /// The inputed line is created.
+    /// The object of SymbolsStorageCreator is created,
+    /// method CreateSumbolsStorage is called and then dictionaries are filled.
+    /// The object of ResultLineCreator is created, 
+    /// method symbolsReplacer is called and symbols in inputed line are replaced.
+    /// </summary>
+    static void Main(string[] args)
+    {
+      try
+      {
+        string replacedLine = Console.ReadLine();
+
+        SymbolsStorageCreator cyrillicsStorage = new SymbolsStorageCreator();
+        Dictionary<string, string> CyrillicsToLatin = cyrillicsStorage.CreateSymbolsStorage(cyrillicsToLatin, latinFromCyrillic);
+        SymbolsStorageCreator latinStorage = new SymbolsStorageCreator();
+        Dictionary<string, string> LatinToCyrillics = latinStorage.CreateSymbolsStorage(latinToCyrillics, cyrillicsFromLatin);
+
+        ResultLineCreator symbolsReplacer = new ResultLineCreator(replacedLine);
+      
+        if (symbolsReplacer.Language(CyrillicsToLatin, LatinToCyrillics) == LanguageChoice.Cyrillics)
+        {
+          replacedLine = symbolsReplacer.ReplacedLine(CyrillicsToLatin);
+        }
+        else if (symbolsReplacer.Language(CyrillicsToLatin, LatinToCyrillics) == LanguageChoice.Latin)
+        {
+          replacedLine = symbolsReplacer.ReplacedLine(LatinToCyrillics);
+        }
+        else if (symbolsReplacer.Language(CyrillicsToLatin, LatinToCyrillics) == LanguageChoice.NonExactlyLanguage)
+        {
+          throw new InvalidLineException(INVALIDLINE);
+        }
+        else if((symbolsReplacer.Language(CyrillicsToLatin, LatinToCyrillics)) == LanguageChoice.InvalidLine ||
+                (symbolsReplacer.Language(CyrillicsToLatin, LatinToCyrillics)) == LanguageChoice.DefaultValue)
+        {
+          throw new FormatException();
+        }
+        Console.WriteLine(replacedLine);
+        Console.ReadKey();
+      }
+      catch (InvalidLineException message)
+      {
+       Console.WriteLine(message);
+      }
+      catch (FileNotFoundException)
+      {
+       Console.WriteLine(FILENOTFOUND);
+      }
+      catch (FormatException)
+      {
+       Console.WriteLine(NONTRASLITLINE);
+      }
+      Console.ReadKey();
+    }
+  }
+}
---
+++ DEV-11/TranslitLinesTransformation/ResultLineCreator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TranslitLinesTransformation
+{
+  /// <summary>
+  /// enum LanguageChoice contains
+  /// markers of language
+  /// </summary>
+  public enum LanguageChoice
+  {
+    Latin,
+    Cyrillics,
+    NonExactlyLanguage,
+    InvalidLine,
+    DefaultValue
+  }
+  /// <summary>
+  /// Class is for creating the result line
+  /// with relaced symbols.
+  /// </summary>
+  class ResultLineCreator
+  {
+    public string InputedLine
+    { get; private set; }
+    public ResultLineCreator(string inputedLine)
+    {
+      InputedLine = inputedLine;
+    }
+    /// <summary>
+    /// Method is for choose the rigth language.
+    /// </summary>
+    /// <param name="CyrillicsToLatin">is dictionary contains cyrrillics symbols as keys and latin as values</param>
+    /// <param name="LatinToCyrillics">is dictionary contains latin symbols as keys and cyrillics as values</param>
+    /// <returns>marker of relevant language</returns>
+    public LanguageChoice Language(Dictionary<string, string> CyrillicsToLatin, Dictionary<string, string> LatinToCyrillics)
+    {
+      LanguageChoice language = LanguageChoice.DefaultValue;
+      if (Regex.IsMatch(InputedLine, "[xqjwXQJW]"))
+      {
+        language = LanguageChoice.InvalidLine;
+      }    
+      else if (Regex.IsMatch(InputedLine, "[a-zA-Z]+"))
+      {
+        language = LanguageChoice.Latin;
+        if (Regex.IsMatch(InputedLine, "[а-яА-Я]+"))
+        {
+          language = LanguageChoice.NonExactlyLanguage;
+        }
+      }
+      else if (Regex.IsMatch(InputedLine, "[а-яА-Я]+"))
+      {
+        language = LanguageChoice.Cyrillics;
+      }
+      return language;
+    }
+
+    /// <summary>
+    /// Method replaces symbols in inputed line.
+    /// </summary>
+    /// <param name="transformatedSymbols">dictionary of transformated symbols</param>
+    /// <returns>line with replaced simbols</returns>
+    public string ReplacedLine(Dictionary<string, string> transformatedSymbols)
+    {
+      foreach (KeyValuePair<string, string> kvp in transformatedSymbols)
+      {
+        InputedLine = InputedLine.Replace(kvp.Key, kvp.Value);
+      }
+      return InputedLine;
+    }
+  }
+}
