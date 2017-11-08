5ac9fa6a2b0806040c3dd6c19866607aede0efef
Nechay Olga <olganechay.bsu@gmail.com>
2017-11-08 18:27:23 +0300

    Test for method to find max value was added

---
+++ DEV-13/StaffRecruitmentTests/BoundsOfQuantityFinderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StaffRecruitment.Tests
+{
+  [TestClass]
+  public class BoundsOfQuantityFinderTests
+  {
+    [TestMethod]
+    public void FindMaxNumbersOfEmployeesFixSumTests_outputedSalary_1800_juniorSalary_1000_18_returned()
+    {
+      //arrange
+      int outputedSalary = 1800;
+      int juniorSalary = 100;
+      int numberOfEmployees = outputedSalary / juniorSalary;
+
+      //act
+      BoundsOfQuantityFinder boundsOfQuantityFinder = new BoundsOfQuantityFinder();
+      boundsOfQuantityFinder.junior.salary = 100;
+      int actual = boundsOfQuantityFinder.FindMaxNumbersOfEmployeesFixSum(outputedSalary);
+      int expected = numberOfEmployees;
+
+      //assert
+      Assert.AreEqual(expected, actual);
+    }
+  }
+}
---
+++ DEV-13/StaffRecruitmentTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StaffRecruitmentTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StaffRecruitmentTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9753f61a-d26f-4e4f-b25e-7f7f3d2f1050")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
